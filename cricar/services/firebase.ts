596ab1e597b8c005c6d6e332edcf1c9d57ee67c8
nikkusharm <ss4897687@gmail.com>
2026-03-26 13:02:41 +0530

    add firebase config

--- cricar/services/firebase.ts
+++ cricar/services/firebase.ts
@@ -6,7 +6,7 @@ import {
 import { getDatabase, ref } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey:            process.env.EXPO_PUBLIC_FIREBASE_API_KEY            || 'your-api-key',
+  apiKey:            process.env.EXPO_PUBLIC_FIREBASE_API_KEY            || 'AIzaSyBAwG2rEWpJWPGFa2FvTwDzuc7aB_5QDIk',
   authDomain:        process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN        || 'your-project.firebaseapp.com',
   projectId:         process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID         || 'your-project-id',
   storageBucket:     process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET     || 'your-project.appspot.com',
