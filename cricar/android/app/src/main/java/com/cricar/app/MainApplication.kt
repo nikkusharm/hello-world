b0401956d13a9d4f7fca931cc3678d6e1a696b60
nikkusharm <ss4897687@gmail.com>
2026-03-28 10:34:07 +0530

    enable new architecture fix missing so file

--- cricar/android/app/src/main/java/com/cricar/app/MainApplication.kt
+++ cricar/android/app/src/main/java/com/cricar/app/MainApplication.kt
@@ -17,15 +17,11 @@ class MainApplication : Application(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost =
       ReactNativeHostWrapper(this, object : DefaultReactNativeHost(this) {
-        override fun getPackages(): List<ReactPackage> =
-            PackageList(this).packages
-
+        override fun getPackages(): List<ReactPackage> = PackageList(this).packages
         override fun getJSMainModuleName(): String = "index"
-
         override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
-
-        override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-        override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+        override val isNewArchEnabled: Boolean = true
+        override val isHermesEnabled: Boolean = true
       })
 
   override val reactHost: ReactHost
@@ -34,9 +30,7 @@ class MainApplication : Application(), ReactApplication {
   override fun onCreate() {
     super.onCreate()
     SoLoader.init(this, false)
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      load()
-    }
+    load()
     ApplicationLifecycleDispatcher.onApplicationCreate(this)
   }
 }
