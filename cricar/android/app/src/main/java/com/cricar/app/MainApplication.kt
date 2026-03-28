382f30533fffd63a407e6a67101e3732e932ce41
nikkusharm <ss4897687@gmail.com>
2026-03-29 02:19:06 +0530

    fix: enable new arch, restore default MainApplication

--- cricar/android/app/src/main/java/com/cricar/app/MainApplication.kt
+++ cricar/android/app/src/main/java/com/cricar/app/MainApplication.kt
@@ -1,3 +1,18 @@
+package com.cricar.app
+
+import android.app.Application
+import com.facebook.react.PackageList
+import com.facebook.react.ReactApplication
+import com.facebook.react.ReactHost
+import com.facebook.react.ReactNativeHost
+import com.facebook.react.ReactPackage
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
+import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
+import com.facebook.react.defaults.DefaultReactNativeHost
+import com.facebook.soloader.SoLoader
+import expo.modules.ApplicationLifecycleDispatcher
+import expo.modules.ReactNativeHostWrapper
+
 class MainApplication : Application(), ReactApplication {
   override val reactNativeHost: ReactNativeHost =
       ReactNativeHostWrapper(this, object : DefaultReactNativeHost(this) {
@@ -7,8 +22,10 @@ class MainApplication : Application(), ReactApplication {
         override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
         override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
       })
+
   override val reactHost: ReactHost
     get() = getDefaultReactHost(applicationContext, reactNativeHost)
+
   override fun onCreate() {
     super.onCreate()
     SoLoader.init(this, false)
